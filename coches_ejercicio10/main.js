f0e10f9847d92354b594627f1239acffcdea50b5
lauglez01 <lauragonzalez33312@gmail.com>
2024-11-25 09:50:24 +0100

    ejercicio10 pero con el 9 del profe

--- coches_ejercicio10/main.js
+++ coches_ejercicio10/main.js
@@ -1,60 +1,30 @@
-import './style.css';
-import { Car } from "./Car.class";
-import { cars } from './mockData';
+import "./style.css";
+import { cars } from "./data/mockData";
+import {
+  promiseParserToClass,
+  promiseFilterByYears,
+  promiseCreateDivCars,
+} from "./utils-promises";
 
-const mapCars = async (array) => {
-  return array.map((item) => {
-    const car = new Car(item.id, item.make);
-    car.year = item.year;
-    car.model = item.model;
-    car.type = item.type;
-    return car;
-  });
+let containerDiv = document.createElement("div");
+containerDiv.classList.add("container");
+
+let h1 = document.createElement("h1");
+h1.textContent = "Coches desde el 2010";
+
+containerDiv.appendChild(h1);
+
+document.body.appendChild(containerDiv);
+
+let getCars = async () => {
+  let carsClass = await promiseParserToClass(cars);
+  let carsfiltedByYear = await promiseFilterByYears(carsClass);
+  let carsDivs = await promiseCreateDivCars(carsfiltedByYear);
+
+  let divBlock = document.createElement("div");
+  divBlock.className = "block";
+  divBlock.append(...carsDivs);
+  containerDiv.appendChild(divBlock);
 };
 
-const filterCars = async (array) => {
-  return array.filter((car) => car.year >= 2010);
-};
-
-const createDivs = async (array) => {
-  return array.map((item) => {
-    const block = document.createElement('div');
-    block.classList.add('block');
-
-    const div = document.createElement('div');
-
-    const makeModel = document.createElement('h3');
-    makeModel.textContent = `${item.make} ${item.model}`;
-    div.appendChild(makeModel);
-
-    const year = document.createElement('p');
-    year.textContent = `Year: ${item.year}`;
-    div.appendChild(year);
-
-    const type = document.createElement('p');
-    type.textContent = `Type: ${item.type}`;
-    div.appendChild(type);
-
-    block.appendChild(div);
-
-    return block;
-  });
-};
-
-const initializeCars = async () => {
-  const container = document.createElement('div');
-  container.classList.add('container');
-
-  const header = document.createElement('h1');
-  header.textContent = 'Car List';
-  container.appendChild(header);  
-  document.body.appendChild(container);
-
-  const mappedCars = await mapCars(cars);
-  const filteredCars = await filterCars(mappedCars);
-  const divs = await createDivs(filteredCars);
-
-  divs.forEach((div) => container.appendChild(div)); 
-};
-
-initializeCars();
+getCars();
---
+++ coches_ejercicio10/utils-promises.js
@@ -0,0 +1,50 @@
+import { Car } from "./classes/Car.class";
+
+export let promiseParserToClass = (literObjectCars) => {
+  return new Promise((resolve, reject) => {
+    if (literObjectCars !== null && literObjectCars !== undefined) {
+      let carsClass = literObjectCars.map((car) => {
+        let newCar = new Car(car.id, car.make);
+        newCar.model = car.model;
+        newCar.type = car.type;
+        newCar.year = car.year;
+        return newCar;
+      });
+      resolve(carsClass);
+    } else {
+      reject("promiseParserToClass: literObjectCars is null or undefined");
+    }
+  });
+};
+
+export let promiseFilterByYears = (cars) => {
+  return new Promise((resolve, reject) => {
+    if (cars !== null && cars !== undefined) {
+      let carsfiltedByYear = cars.filter((car) => {
+        return car.year >= 2010;
+      });
+      resolve(carsfiltedByYear);
+    } else {
+      reject("promiseFilterByYears: cars is null or undefined");
+    }
+  });
+};
+
+export let promiseCreateDivCars = (cars) => {
+  return new Promise((resolve, reject) => {
+    if (cars !== null && cars !== undefined) {
+      let carsDivs = cars.map((car) => {
+        let divs = document.createElement("div");
+        let modelMakeParagraph = document.createElement("p");
+        modelMakeParagraph.textContent = `Model: ${car.model}, Make: ${car.make}`;
+        let typeYearParagraph = document.createElement("p");
+        typeYearParagraph.textContent = `Type: ${car.type}, Year: ${car.year}`;
+        divs.append(modelMakeParagraph, typeYearParagraph);
+        return divs;
+      });
+      resolve(carsDivs);
+    } else {
+      reject("promiseCreateDivCars: cars is null or undefined");
+    }
+  });
+};
